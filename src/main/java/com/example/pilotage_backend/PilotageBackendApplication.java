4dc4211971e9af18bf57153313ec8711d364378d
hamed <hamedkounikorgo60@gmail.com>
2026-06-27 20:00:49 +0000

    Initialisation du backend Spring Boot

---
+++ src/main/java/com/example/pilotage_backend/PilotageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pilotage_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotageBackendApplication.class, args);
+	}
+
+}
